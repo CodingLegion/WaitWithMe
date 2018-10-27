d6d3b7fe7025d84d041046d70148808a1854d143
angel_a_petrov2 <angel.a.petrov2@gmail.com>
2018-10-27 21:49:26 +0100

    login page'

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
-
+import Main from '../screens/MainScreen';
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  Main: Main,
 });
